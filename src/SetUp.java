a69db6ee6b3d522fe9a0f972b0b022139beedad1
wifec <olga.alonina@gmail.com>
2022-04-28 12:40:40 -0400

    practise tue 04/26/2022 AO

--- src/SetUp.java
+++ src/SetUp.java
@@ -2,4 +2,45 @@ public class SetUp {
     // Setting up project for everyone
     //Alonina Olga
     //make more changes
+    // changes made by cristina
+    //try to practice git with the group
+
+    // more changes
+    // hello world
+    // updates from Mariia
+    // need more updates
+
+    // Monday 04/18/2022
+    // extra session
+
+    // Mariia 04/21/2022
+
+    // new update from Jamil
+
+    // raluca made some changes
+    // more changes by me
+
+     // ayse added some comment
+
+    // Created semih branch
+
+    // Jamil branch
+    //My new branch created.
+
+    //  Mary update
+
+    // new updates from Grace 4/21/22
+
+    // updated by ayse
+
+
+
+    // updated 04/23
+    // srdjan
+    //Yasemin 04/22
+
+    // update again by Grace 4/22/22
+    //dfksjhdfkjshdjfk56666
+
+
 }
